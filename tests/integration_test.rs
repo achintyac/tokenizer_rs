bfb36b8d67208744558aef2728465024c21f8fd3
achintyac <achintya.chaudhari@gmail.com>
2024-05-13 14:40:33 -0500

    Reorg module

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,29 +1,29 @@
 use std::path::PathBuf;
 use std::fs;
 
-use tokenizer_rs;
+use tokenizer_rs::tokenizer;
 
 #[test]
 fn full_run_through() {
-    let paths = tokenizer_rs::Path {
+    let paths = tokenizer::Path {
         data: PathBuf::from("./tests/foo.txt"),
         merges: PathBuf::from("./tests/merges.txt"),
     };
 
-    let vocab = tokenizer_rs::Vocab {
+    let vocab = tokenizer::Vocab {
         size: 280
     };
 
-    let tokenizer = tokenizer_rs::BaseTokenizer {
+    let tokenizer = tokenizer::BaseTokenizer {
         path: paths,
         vocab: vocab
     };
 
-    let initial_tokens = tokenizer_rs::BaseTokenizer::read_to_bytes(&tokenizer.path.data);
-    let contents = tokenizer_rs::BaseTokenizer::convert_to_u32(initial_tokens);
-    let (contents, merges) = tokenizer_rs::BaseTokenizer::encode(contents, tokenizer.vocab);
-    let contents_new = tokenizer_rs::BaseTokenizer::decode(&contents, &merges);
-    let ans = tokenizer_rs::BaseTokenizer::convert_to_u8(contents_new);
+    let initial_tokens = tokenizer::BaseTokenizer::read_to_bytes(&tokenizer.path.data);
+    let contents = tokenizer::BaseTokenizer::convert_to_u32(initial_tokens);
+    let (contents, merges) = tokenizer::BaseTokenizer::encode(contents, tokenizer.vocab);
+    let contents_new = tokenizer::BaseTokenizer::decode(&contents, &merges);
+    let ans = tokenizer::BaseTokenizer::convert_to_u8(contents_new);
 
     let original_contents = fs::read_to_string(tokenizer.path.data).expect("Should have been a file here to read");
     let final_converted_ans = String::from_utf8(ans).unwrap();
